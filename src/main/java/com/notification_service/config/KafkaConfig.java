0b30d63838559a4d45f0eb0c29957f8447c6b4e8
Shakil Ahmad <shakil@spark.love>
2026-05-10 13:05:46 +0530

    added the dependency and resolved the issue of kafka and zookeeper

--- src/main/java/com/notification_service/config/KafkaConfig.java
+++ src/main/java/com/notification_service/config/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.notification_service.config;
 import com.notification_service.kafka.event.OrderPlacedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -16,6 +17,9 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, OrderPlacedEvent> consumerFactory() {
 
@@ -26,7 +30,7 @@ public class KafkaConfig {
 
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
--- src/main/java/com/notification_service/service/NotificationService.java
+++ src/main/java/com/notification_service/service/NotificationService.java
@@ -1,13 +1,15 @@
 package com.notification_service.service;
 
 import com.notification_service.kafka.event.OrderPlacedEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class NotificationService {
 
     public void sendNotification(OrderPlacedEvent event) {
-        System.out.println("📧 Sending Email to: " + event.getUserEmail());
-        System.out.println("💰 Order Amount: " + event.getAmount());
+        log.info("📧 Sending Email notification to: {}", event.getUserEmail());
+        log.info("💰 Order Amount: {}", event.getAmount());
     }
 }
